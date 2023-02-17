6661a8e2c99fb368c49e64852798619f9861e726
adamrodrigues11 <adamrodrigues11@gmail.com>
2023-02-17 11:09:06 -0800

    updated artistRepo to match new db

--- Repositories/ArtistRepo.cs
+++ Repositories/ArtistRepo.cs
@@ -13,7 +13,7 @@ namespace TeamRedInternalProject.Repositories
 
         public List<Artist> GetArtistsAtCurrentFestival()
         {
-            Festival currentFestival = _db.Festivals.Where(f => f.FestivalId == 1).First();
+            Festival currentFestival = _db.Festivals.Where(f => f.IsCurrent).First();
             List<Artist> cfArtists = _db.Artists.Where(a => a.Festivals.Contains(currentFestival)).ToList();
             return cfArtists;
         }
@@ -23,56 +23,57 @@ namespace TeamRedInternalProject.Repositories
             return _db.Artists.ToList();
         }
 
-        public List<Artist> GetArtists()
-        {
-            
-
-            //var artistList = from a in _db.Artists
-            //                 join f in _db.FestivalArtists on a.ArtistId equals f.FestivalId where a.ArtistId == 
-            int currentFestivalId = _db.CurrentFestivals.FirstOrDefault().FestivalId;
-            //var currentFestivalArtistIds = _db.Artists.Join(_db.Festivals, a => a.ArtistId, f => f.FestivalId, (a, f) => new { a.ArtistId, f.FestivalId})
-            //                                           .Where(fa => fa.FestivalId == currentFestivalId)
-            //                                           .Select( l => new { ArtistId = l.ArtistId, FestivalId = l.FestivalId }).ToList();
+        //public List<Artist> GetArtists()
+        //{
 
 
-            List<int> artistIds = _db.Artists.Select(a => a.ArtistId).ToList();
+        //    //var artistList = from a in _db.Artists
+        //    //                 join f in _db.FestivalArtists on a.ArtistId equals f.FestivalId where a.ArtistId == 
+        //    //int currentFestivalId = _db.CurrentFestivals.FirstOrDefault().FestivalId;
+        //    //var currentFestivalArtistIds = _db.Artists.Join(_db.Festivals, a => a.ArtistId, f => f.FestivalId, (a, f) => new { a.ArtistId, f.FestivalId})
+        //    //                                           .Where(fa => fa.FestivalId == currentFestivalId)
+        //    //                                           .Select( l => new { ArtistId = l.ArtistId, FestivalId = l.FestivalId }).ToList();
 
-            //int[,] artistIds = new int[,] { };
-            var artists = from a in _db.Artists
-                          from f in _db.Festivals
-                          where artistIds.Contains(f.FestivalId) && f.FestivalId.Equals(currentFestivalId)
-                          select a;
-            
-            //var currentArtistIds = from f in _db.Festivals
-            //                       where (from a in f.Artists where artistIds )
 
-            List < Artist > currentArtists = new List<Artist>();
+        //    List<int> artistIds = _db.Artists.Select(a => a.ArtistId).ToList();
 
-            //opt 1
-            //foreach (int id in currentFestivalArtistIds){
-            //    currentArtists.Add(_db.Artists.Where(a => a.ArtistId == id).FirstOrDefault);
+        //    //int[,] artistIds = new int[,] { };
+        //    var artists = from a in _db.Artists
+        //                  from f in _db.Festivals
+        //                  where artistIds.Contains(f.FestivalId) && f.FestivalId.Equals(currentFestivalId)
+        //                  select a;
 
-                
-            //}
+        //    //var currentArtistIds = from f in _db.Festivals
+        //    //                       where (from a in f.Artists where artistIds )
 
-            //opt 2
-            //IEnumerable < Artist > results = _db.Artists.Where(currentFestivalArtistIds.ToList().Contains(a => a.ArtistId));
+        //    List < Artist > currentArtists = new List<Artist>();
 
-            //var festivalArtists = _db.Festivals
-    //.Join(_db.FestivalArtists,
-    //    f => new { f.Id, ArtistId = f.Id },
-    //    fa => new { fa.FestivalId, fa.ArtistId },
-    //    (f, fa) => fa)
-    //.Join(_db.Artists,
-    //    fa => fa.ArtistId,
-    //    a => a.Id,
-    //    (fa, a) => new { FestivalArtist = fa, Artist = a })
-    //.Where(x => x.FestivalArtist.FestivalId == festivalId && x.Artist.Id == artistId)
-    //.Select(x => x.Artist)
-    //.ToList();
+        //    //opt 1
+        //    //foreach (int id in currentFestivalArtistIds){
+        //    //    currentArtists.Add(_db.Artists.Where(a => a.ArtistId == id).FirstOrDefault);
 
-            return currentArtists;
-        }
+
+        //    //}
+
+        //opt 2
+        //IEnumerable < Artist > results = _db.Artists.Where(currentFestivalArtistIds.ToList().Contains(a => a.ArtistId));
+
+        //var festivalArtists = _db.Festivals
+        //.Join(_db.FestivalArtists,
+        //    f => new { f.Id, ArtistId = f.Id },
+        //    fa => new { fa.FestivalId, fa.ArtistId },
+        //    (f, fa) => fa)
+        //.Join(_db.Artists,
+        //    fa => fa.ArtistId,
+        //    a => a.Id,
+        //    (fa, a) => new { FestivalArtist = fa, Artist = a })
+        //.Where(x => x.FestivalArtist.FestivalId == festivalId && x.Artist.Id == artistId)
+        //.Select(x => x.Artist)
+        //.ToList();
+
+        //            return currentArtists;
+        //        }
+        //    }
     }
 }
 //var userRoles = (from u in _context.Users
