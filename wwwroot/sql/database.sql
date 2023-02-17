eb5e5a347c2ba0d4f3ea1b7dda0c05161d457704
adamrodrigues11 <adamrodrigues11@gmail.com>
2023-02-17 11:06:05 -0800

    removed CurrentFestival table and added a boolean column to the Festival table. Added an image path to artist. Rebuilt the database.sql script so it can be run by all.

--- wwwroot/sql/database.sql
+++ wwwroot/sql/database.sql
@@ -10,9 +10,9 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS Ticket;
 DROP TABLE IF EXISTS FestivalTicketType;
 DROP TABLE IF EXISTS TicketType;
+DROP TABLE IF EXISTS CurrentFestival;
 DROP TABLE IF EXISTS FestivalArtist;
 DROP TABLE IF EXISTS Artist;
-DROP TABLE IF EXISTS CurrentFestival;
 DROP TABLE IF EXISTS Festival;
 DROP TABLE IF EXISTS [Order];
 DROP TABLE IF EXISTS [User];
@@ -35,18 +35,15 @@ CREATE TABLE [Order] (
 CREATE TABLE Festival (
 	festivalID		INT IDENTITY	PRIMARY KEY,
 	[date]			DATE			NOT NULL,
-	[location]		VARCHAR(255)	NOT NULL
-);
-
-CREATE TABLE CurrentFestival (
-	festivalID		INT				PRIMARY KEY,
-	FOREIGN KEY (festivalID)		REFERENCES Festival(festivalID)
+	[location]		VARCHAR(255)	NOT NULL,
+	isCurrent		BIT				NOT NULL
 );
 
 CREATE TABLE Artist (
 	artistID		INT IDENTITY	PRIMARY KEY,
 	artistName		VARCHAR(255)	NOT NULL,
-	artistBio		VARCHAR(4095)
+	artistBio		VARCHAR(4095),
+	imgPath			VARCHAR(255)
 );
 
 CREATE TABLE FestivalArtist (
@@ -88,19 +85,16 @@ CREATE TABLE Ticket (
 INSERT INTO TicketType VALUES('General Admission', 50.00);
 
 /* Seed Artist */
-INSERT INTO Artist VALUES('Billy Talent', 'sample description');
-INSERT INTO Artist VALUES('The Wiggles', 'sample description');
-INSERT INTO Artist VALUES('Sum 41', 'sample description');
-INSERT INTO Artist VALUES('Drake', 'sample description');
-INSERT INTO Artist VALUES('Prince (Hologram)', 'sample description');
-INSERT INTO Artist VALUES('Beethoven', 'sample description');
-INSERT INTO Artist VALUES('Toto', 'sample description');
+INSERT INTO Artist (artistName, artistBio) VALUES('Billy Talent', 'sample description');
+INSERT INTO Artist (artistName, artistBio) VALUES('The Wiggles', 'sample description');
+INSERT INTO Artist (artistName, artistBio) VALUES('Sum 41', 'sample description');
+INSERT INTO Artist (artistName, artistBio) VALUES('Drake', 'sample description');
+INSERT INTO Artist (artistName, artistBio) VALUES('Prince (Hologram)', 'sample description');
+INSERT INTO Artist (artistName, artistBio) VALUES('Beethoven', 'sample description');
+INSERT INTO Artist (artistName, artistBio) VALUES('Toto', 'sample description');
 
 /* Seed Festival */
-INSERT INTO Festival VALUES(CONVERT(DATE, '06/03/2023', 101), 'Vancouver');
-
-/* Seed CurrentFestival */
-INSERT INTO CurrentFestival VALUES(1);
+INSERT INTO Festival VALUES(CONVERT(DATE, '06/03/2023', 101), 'Vancouver', 1);
 
 /* Seed FestivalTicketType */
 INSERT INTO FestivalTicketType VALUES(1, 1, 20000);
